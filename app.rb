61d750434bceeaf5ad98c0ecadb1ee87160124a3
daniel <daniel_ub@live.com>
2019-06-25 18:55:11 -0500

    comentarios

--- app.rb
+++ app.rb
@@ -2,3 +2,46 @@ require 'sinatra'
 require 'sinatra/activerecord'
 set :database, "sqlite3:project-name.sqlite3"
 Dir[__dir__+"/models/*.rb"].each {|file| require file }
+
+
+people=[]
+
+persona1 = Person.new("daniel", "bolivar", 27) #intansiar es crear un objeto
+
+people << persona1
+
+get '/people' do  #index
+        @people = people
+        erb :index
+end
+
+get '/people/:id' do
+    #show
+    index=params[:id].to_i
+    @person=people[index]
+    erb :show
+end
+
+get '/people/new' do
+    #new
+end
+
+post '/people' do
+    #create
+end
+
+get '/people/:id/edit' do
+    #edit
+end
+
+put '/people/' do
+    #update
+end
+
+patch '/people/' do
+    #update
+end
+
+delete '/people/' do
+    #destroy
+end
---
+++ models/person.rb
@@ -0,0 +1,12 @@
+
+class Person
+    def initialize(nombre, apellido, edad)
+        @nombre = nombre
+        @apellido = apellido
+        @edad = edad        
+    end
+    def nombre
+        @nombre
+    end
+end
+
